d39dcef985fba078355db3776af30ab558814665
Youcef <youcefcopy@gmail.com>
2026-02-13 18:05:25 +0100

    a bug in adding a new student

--- src/components/exercice2/MyClass.vue
+++ src/components/exercice2/MyClass.vue
@@ -9,6 +9,15 @@ const students = ref([
   { name: 'Charlie', scores: [95, 98, 100, 97] },
   { name: 'Diana', scores: [60, 55, 58, 62] },
 ])
+
+// more students
+const name = ref('')
+const addStudent = () => {
+  if (name.value.trim() !== '') {
+    students.value.push({ name: name, scores: [] })
+  }
+  name.value = ''
+}
 </script>
 
 <template>
@@ -16,5 +25,22 @@ const students = ref([
     <div class="student">
       <MyStudent v-for="student in students" :key="student.name" :student="student" />
     </div>
+    <div class="admin">
+      <h2>Enter a new Student:</h2>
+      <label for="name">Name: </label>
+      <input type="text" id="name" v-model="name" />
+      <button @click="addStudent">Add Student</button>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.students {
+  display: flex;
+}
+
+.student {
+  width: 50%;
+  background-color: seagreen;
+}
+</style>
