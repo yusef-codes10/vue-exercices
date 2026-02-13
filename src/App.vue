774e115138cedde15960b43bf102dd7db4207305
Youcef <youcefcopy@gmail.com>
2026-02-13 17:07:39 +0100

    students and thier grades

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import ExerciceOne from './components/ExerciceOne.vue'
+import ExerciceTwo from './components/ExerciceTwo.vue'
 </script>
 
 <template>
   <h1>You did it!</h1>
   <ExerciceOne />
+  <ExerciceTwo />
 </template>
 
 <style scoped></style>
--- src/components/ExerciceTwo.vue
+++ src/components/ExerciceTwo.vue
@@ -14,10 +14,27 @@
 // passingStudents - Students with average >= 70
 // classAverage - Average of all students' averages
 // gradeDistribution - Count of A's, B's, C's, D's, F's (A: 90+, B: 80-89, etc.)
+
+import { ref } from 'vue'
+
+const students = ref([
+  { name: 'Alice', scores: [85, 92, 78, 90] },
+  { name: 'Bob', scores: [70, 65, 80, 75] },
+  { name: 'Charlie', scores: [95, 98, 100, 97] },
+  { name: 'Diana', scores: [60, 55, 58, 62] },
+])
 </script>
 
 <template>
-  <h1>You did it!</h1>
+  <div class="student">
+    <ul v-for="student in students" :key="student.name">
+      <li>Name: {{ student.name }}</li>
+      <ul>
+        Grades:
+        <li v-for="(grade, index) in student.scores" :key="index">{{ grade }}</li>
+      </ul>
+    </ul>
+  </div>
 </template>
 
 <style scoped></style>
